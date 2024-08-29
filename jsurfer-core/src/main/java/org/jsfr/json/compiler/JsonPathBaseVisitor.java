4843ec1bf2ae6fa4fc5fc460ca7a78f4fd561024
Flash Gordon <>
2024-08-29 10:36:39 +0000

    Next version is 0.13-SNAPSHOT

--- jsurfer-core/src/main/java/org/jsfr/json/compiler/JsonPathBaseVisitor.java
+++ jsurfer-core/src/main/java/org/jsfr/json/compiler/JsonPathBaseVisitor.java
@@ -1,4 +1,4 @@
-// Generated from JsonPath.g4 by ANTLR 4.13.1
+// Generated from JsonPath.g4 by ANTLR 4.13.2
 
 package org.jsfr.json.compiler;
 
--- jsurfer-core/src/main/java/org/jsfr/json/compiler/JsonPathLexer.java
+++ jsurfer-core/src/main/java/org/jsfr/json/compiler/JsonPathLexer.java
@@ -1,4 +1,4 @@
-// Generated from JsonPath.g4 by ANTLR 4.13.1
+// Generated from JsonPath.g4 by ANTLR 4.13.2
 
 package org.jsfr.json.compiler;
 
@@ -13,7 +13,7 @@ import org.antlr.v4.runtime.misc.*;
 
 @SuppressWarnings({"all", "warnings", "unchecked", "unused", "cast", "CheckReturnValue", "this-escape"})
 public class JsonPathLexer extends Lexer {
-	static { RuntimeMetaData.checkVersion("4.13.1", RuntimeMetaData.VERSION); }
+	static { RuntimeMetaData.checkVersion("4.13.2", RuntimeMetaData.VERSION); }
 
 	protected static final DFA[] _decisionToDFA;
 	protected static final PredictionContextCache _sharedContextCache =
